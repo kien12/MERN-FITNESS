d1b5fc4e8136991c9aa852478eeaedc58741f562
Kien Nguyen <nguyen_kien@live.com>
2023-05-12 23:10:12 -0700

    work on recipe component

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -2,7 +2,7 @@ export const CreateRecipe = () => {
   console.log(data)
   return ( 
     <div className="create-recipe">
-       <h2>create recipeee</h2>
+       <h2>create recipeeee</h2>
     </div>
   ) 
 }
